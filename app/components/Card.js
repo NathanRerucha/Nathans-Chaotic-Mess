0286f1c8ca805fe208e589743140510349fbbf7f
nate <nrerucha001@student.butte.edu>
2023-12-04 22:45:37 -0800

    fixed the image issue

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export default function Card({
   img = 'http://placekitten.com/g/200/300',
   title = 'Fake Title',
@@ -9,7 +7,7 @@ export default function Card({
 }) {
   return (
     <div className="my-5 shadow-lg w-72 max-w-md p-2 rounded-[20px] overflow-hidden bg-white hover:scale-105 transition-all duration-200 active:bg-gray-300">
-      <Image className="h-48 rounded w-half shadow-md m-auto" src={img} />
+      <img className="h-48 rounded w-half shadow-md m-auto" src={img} alt="Meaningful alt text" />
       <h1 className="font-bold text-xl mb-1">{title}</h1>
       <h2 className="text-gray-500 mb-4">{subtitle}</h2>
       <p className="p-1 mb-3 text-gray-900 text-base border border-solid rounded-lg border-slate-500">{description}</p>
